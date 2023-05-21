62fe9a015c60a5dffa07257d8f1448f9a068ce01
Justice Nyaga <justiceevansmwema@gmail.com>
2023-05-21 17:54:12 +0300

    update Login Page and Register Page

--- frontend/src/utils/imageUrls.js
+++ frontend/src/utils/imageUrls.js
@@ -1,16 +1,16 @@
 const logoUrl =
   process.env.NODE_ENV === "production"
-    ? "https://proeshopstorage.blob.core.windows.net/proshopcontainer/logo.png"
+    ? "https://proshop.blob.core.windows.net/proshop-container/logo.png"
     : "/images/logo.png";
 
 const notFoundUrl =
   process.env.NODE_ENV === "production"
-    ? "https://proeshopstorage.blob.core.windows.net/proshopcontainer/404.png"
+    ? "https://proshop.blob.core.windows.net/proshop-container/404.png"
     : "/images/404.png";
 
 const commentImageUrl =
   process.env.NODE_ENV === "production"
-    ? "https://proeshopstorage.blob.core.windows.net/proshopcontainer/no-comment.png"
+    ? "https://proshop.blob.core.windows.net/proshop-container/no-comment.png"
     : "/images/no-comment.png";
 
 export { logoUrl, notFoundUrl, commentImageUrl };
